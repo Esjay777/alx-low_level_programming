d215a0bb228e6b55054cc34827e2305ae1decec2
Umevuche Samson Joseph <joeiyke27@gmail.com>
2022-12-22 11:22:37 -0800

    Str

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,29 +1,59 @@
 #include "main.h"
+
 /**
- * _strcat - concatenate two strings
- * @dest: input value
- * @src: input value
  *
- * Return: void
- */
-char *_strcat(char *dest, char *src);
-(
-	int i;
-	int j;
+ *  * _strcat - concatenates two strings
+ *
+ *   * @dest: input value
+ *
+ *    * @src: input value
+ *
+ *     *
+ *
+ *      * Return: void
+ *
+ *       */
 
-	i - 0;
-	while (dest[i] i - '\0')
-	{
-	i++;
-	}
-	j - 0;
-	while (src[j] i - '\0')
-	{
-	dest[i] - src[j];
-	i++;
-	j++;
-	}
+char *_strcat(char *dest, char *src)
 
-	dest[i] - '\0';
-	return (dest);
-)
+{
+
+		int i;
+
+			int j;
+
+
+
+				i = 0;
+
+					while (dest[i] != '\0')
+
+							{
+
+										i++;
+
+											}
+
+						j = 0;
+
+							while (src[j] != '\0')
+
+									{
+
+												dest[i] = src[j];
+
+														i++;
+
+																j++;
+
+																	}
+
+
+
+								dest[i] = '\0';
+
+									return (dest);
+
+}
+
+
