154a802e5a0419195b98fdfee469125e8bca2b31
Umevuche Samson Joseph <joeiyke27@gmail.com>
2022-12-27 04:17:34 -0800

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,35 +1,51 @@
-#include "main.h"
-
-
+#include "holberton.h"
 
 /**
- *
- *  * main - check the code for ALX School students.
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
-
-int main(void)
-
+ *  * print_times_table - Prints a multiplication table up to param
+ *   * @n: The number to be treated
+ *    *
+ *     * Return: Number matrix
+ *      */
+void print_times_table(int n)
 {
+		int x, y, z;
 
-	    print_times_table(3);
-
-	        _putchar('\n');
-
-		    print_times_table(5);
-
-		        _putchar('\n');
-
-			    print_times_table(98);
-
-			        _putchar('\n');
-
-				    print_times_table(12);  
-
-				        return (0);
-
+			if (n >= 0 && n <= 14)
+					{
+								for (x = 0; x <= n; x++)
+											{
+															for (y = 0; y <= n; y++)
+																			{
+																								z = x * y;
+																												if (z > 99)
+																																	{
+																																							_putchar(',');
+																																												_putchar(32);
+																																																	_putchar((z / 100) + '0');
+																																																						_putchar(((z / 10) % 10) + '0');
+																																																											_putchar((z % 10) + '0');
+																																																															}
+																																else if (z > 9)
+																																					{
+																																											_putchar(',');
+																																																_putchar(32);
+																																																					_putchar(32);
+																																																										_putchar(((z / 10) % 10) + '0');
+																																																															_putchar((z % 10) + '0');
+																																																																			}
+																																				else
+																																									{
+																																															if (y != 0)
+																																																					{
+																																																												_putchar(',');
+																																																																		_putchar(32);
+																																																																								_putchar(32);
+																																																																														_putchar(32);
+																																																																																			}
+																																																				_putchar(z + '0');
+																																																								}
+																																							}
+																		_putchar('\n');
+																				}
+									}
 }
